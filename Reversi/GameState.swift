eda46d447880e774c41a615daf91f7264ca99aee
Tomochika Hara <git@thara.dev>
2020-04-25 00:31:14 +0900

    :art: Move countDisks/sideWithMoreDisks to GameState from VC

--- Reversi/GameState.swift
+++ Reversi/GameState.swift
@@ -105,6 +105,36 @@ class GameState {
         
         return players.compactMap { $0 }
     }
+    
+    /// `side` で指定された色のディスクが盤上に置かれている枚数を返します。
+    /// - Parameter side: 数えるディスクの色です。
+    /// - Returns: `side` で指定された色のディスクの、盤上の枚数です。
+    func countDisks(of side: Disk) -> Int {
+        var count = 0
+        
+        for y in boardView.yRange {
+            for x in boardView.xRange {
+                if boardView.diskAt(x: x, y: y) == side {
+                    count +=  1
+                }
+            }
+        }
+        
+        return count
+    }
+    
+    /// 盤上に置かれたディスクの枚数が多い方の色を返します。
+    /// 引き分けの場合は `nil` が返されます。
+    /// - Returns: 盤上に置かれたディスクの枚数が多い方の色です。引き分けの場合は `nil` を返します。
+    func sideWithMoreDisks() -> Disk? {
+        let darkCount = countDisks(of: .dark)
+        let lightCount = countDisks(of: .light)
+        if darkCount == lightCount {
+            return nil
+        } else {
+            return darkCount > lightCount ? .dark : .light
+        }
+    }
 }
 
 enum FileIOError: Error {
