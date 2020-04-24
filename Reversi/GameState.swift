95ece34fdf791e8c499e47e72dbf496be816259a
Tomochika Hara <git@thara.dev>
2020-04-24 23:50:49 +0900

    :art: Separate persistence process from VC

---
+++ Reversi/GameState.swift
@@ -0,0 +1,112 @@
+//
+//  GameState.swift
+//  Reversi
+//
+//  Created by thara on 2020/04/24.
+//  Copyright © 2020 Yuta Koshizawa. All rights reserved.
+//
+
+import Foundation
+
+enum Player: Int {
+    case manual = 0
+    case computer = 1
+}
+
+class GameState {
+    private var turn: Disk? = .dark
+    
+    var boardView: BoardView!
+    
+    /// ゲームの状態を初期化し、新しいゲームを開始します。
+    func newGame() {
+        boardView.reset()
+        turn = .dark
+    }
+    
+    /// ゲームの状態をファイルに書き出し、保存します。
+    func saveGame(at path: String, players: [String]) throws {
+        var output: String = ""
+        output += turn.symbol
+        for side in Disk.sides {
+            output += players[side.index]
+        }
+        output += "\n"
+        
+        for y in boardView.yRange {
+            for x in boardView.xRange {
+                output += boardView.diskAt(x: x, y: y).symbol
+            }
+            output += "\n"
+        }
+        
+        do {
+            try output.write(toFile: path, atomically: true, encoding: .utf8)
+        } catch let error {
+            throw FileIOError.read(path: path, cause: error)
+        }
+    }
+    
+    /// ゲームの状態をファイルから読み込み、復元します。
+    func loadGame(from path: String) throws -> [Player] {
+        let input = try String(contentsOfFile: path, encoding: .utf8)
+        var lines: ArraySlice<Substring> = input.split(separator: "\n")[...]
+        
+        guard var line = lines.popFirst() else {
+            throw FileIOError.read(path: path, cause: nil)
+        }
+        
+        do { // turn
+            guard
+                let diskSymbol = line.popFirst(),
+                let disk = Optional<Disk>(symbol: diskSymbol.description)
+            else {
+                throw FileIOError.read(path: path, cause: nil)
+            }
+            turn = disk
+        }
+
+        // players
+        var players = [Player?](repeating: nil, count: Disk.sides.count)
+        for side in Disk.sides {
+            guard
+                let playerSymbol = line.popFirst(),
+                let playerNumber = Int(playerSymbol.description),
+                let player = Player(rawValue: playerNumber)
+            else {
+                throw FileIOError.read(path: path, cause: nil)
+            }
+            players[side.index] = player
+        }
+
+        do { // board
+            guard lines.count == boardView.height else {
+                throw FileIOError.read(path: path, cause: nil)
+            }
+            
+            var y = 0
+            while let line = lines.popFirst() {
+                var x = 0
+                for character in line {
+                    let disk = Disk?(symbol: "\(character)").flatMap { $0 }
+                    boardView.setDisk(disk, atX: x, y: y, animated: false)
+                    x += 1
+                }
+                guard x == boardView.width else {
+                    throw FileIOError.read(path: path, cause: nil)
+                }
+                y += 1
+            }
+            guard y == boardView.height else {
+                throw FileIOError.read(path: path, cause: nil)
+            }
+        }
+        
+        return players.compactMap { $0 }
+    }
+}
+
+enum FileIOError: Error {
+    case write(path: String, cause: Error?)
+    case read(path: String, cause: Error?)
+}
--- Reversi/ViewController.swift
+++ Reversi/ViewController.swift
@@ -26,8 +26,12 @@ class ViewController: UIViewController {
     
     private var playerCancellers: [Disk: Canceller] = [:]
     
+    private let gameState = GameState()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        gameState.boardView = boardView
         
         boardView.delegate = self
         messageDiskSize = messageDiskSizeConstraint.constant
@@ -223,8 +227,10 @@ extension ViewController {
 // MARK: Game management
 
 extension ViewController {
+    
     /// ゲームの状態を初期化し、新しいゲームを開始します。
     func newGame() {
+        //TODO gameState.newGate()
         boardView.reset()
         turn = .dark
         
@@ -410,99 +416,23 @@ extension ViewController {
     
     /// ゲームの状態をファイルに書き出し、保存します。
     func saveGame() throws {
-        var output: String = ""
-        output += turn.symbol
-        for side in Disk.sides {
-            output += playerControls[side.index].selectedSegmentIndex.description
-        }
-        output += "\n"
-        
-        for y in boardView.yRange {
-            for x in boardView.xRange {
-                output += boardView.diskAt(x: x, y: y).symbol
-            }
-            output += "\n"
-        }
-        
-        do {
-            try output.write(toFile: path, atomically: true, encoding: .utf8)
-        } catch let error {
-            throw FileIOError.read(path: path, cause: error)
-        }
+        let players = playerControls.map { $0.selectedSegmentIndex.description }
+        try gameState.saveGame(at: path, players: players)
     }
     
     /// ゲームの状態をファイルから読み込み、復元します。
     func loadGame() throws {
-        let input = try String(contentsOfFile: path, encoding: .utf8)
-        var lines: ArraySlice<Substring> = input.split(separator: "\n")[...]
-        
-        guard var line = lines.popFirst() else {
-            throw FileIOError.read(path: path, cause: nil)
-        }
-        
-        do { // turn
-            guard
-                let diskSymbol = line.popFirst(),
-                let disk = Optional<Disk>(symbol: diskSymbol.description)
-            else {
-                throw FileIOError.read(path: path, cause: nil)
-            }
-            turn = disk
-        }
-
-        // players
-        for side in Disk.sides {
-            guard
-                let playerSymbol = line.popFirst(),
-                let playerNumber = Int(playerSymbol.description),
-                let player = Player(rawValue: playerNumber)
-            else {
-                throw FileIOError.read(path: path, cause: nil)
-            }
-            playerControls[side.index].selectedSegmentIndex = player.rawValue
-        }
-
-        do { // board
-            guard lines.count == boardView.height else {
-                throw FileIOError.read(path: path, cause: nil)
-            }
-            
-            var y = 0
-            while let line = lines.popFirst() {
-                var x = 0
-                for character in line {
-                    let disk = Disk?(symbol: "\(character)").flatMap { $0 }
-                    boardView.setDisk(disk, atX: x, y: y, animated: false)
-                    x += 1
-                }
-                guard x == boardView.width else {
-                    throw FileIOError.read(path: path, cause: nil)
-                }
-                y += 1
-            }
-            guard y == boardView.height else {
-                throw FileIOError.read(path: path, cause: nil)
-            }
+        let players = try gameState.loadGame(from: path)
+        for (i, player) in players.enumerated() {
+            playerControls[i].selectedSegmentIndex = player.rawValue
         }
 
         updateMessageViews()
         updateCountLabels()
     }
-    
-    enum FileIOError: Error {
-        case write(path: String, cause: Error?)
-        case read(path: String, cause: Error?)
-    }
 }
 
 // MARK: Additional types
-
-extension ViewController {
-    enum Player: Int {
-        case manual = 0
-        case computer = 1
-    }
-}
 
 final class Canceller {
     private(set) var isCancelled: Bool = false
@@ -547,7 +477,7 @@ extension Disk {
 }
 
 extension Optional where Wrapped == Disk {
-    fileprivate init?<S: StringProtocol>(symbol: S) {
+    init?<S: StringProtocol>(symbol: S) {
         switch symbol {
         case "x":
             self = .some(.dark)
@@ -560,7 +490,7 @@ extension Optional where Wrapped == Disk {
         }
     }
     
-    fileprivate var symbol: String {
+    var symbol: String {
         switch self {
         case .some(.dark):
             return "x"
